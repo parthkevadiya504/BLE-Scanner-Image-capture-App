24c29b20a1ab9a9f909557da3f4bd90b2bb9a33d
parth_kevadiya <kevadiyaparth52004@gmail.com>
2024-07-28 15:49:52 +0530

    ble scan updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,86 @@
-
-
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
+
+import 'Controller/ble_controller.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class _MyAppState extends State<MyApp> {
-  List<BluetoothDevice> devices = [];
-
-  @override
-  void initState() {
-    super.initState();
-    //scanForDevices();
-    check();
-  }
-
-  void check() async {
-    if (await FlutterBluePlus.isSupported == false) {
-      print("Bluetooth not supported by this device");
-      return;
-    }
-    else{
-      print("supported device");
-    }
-
-    var subscription = FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
-      print(state);
-      if (state == BluetoothAdapterState.on) {
-        // usually start scanning, connecting, etc
-      } else {
-        // show an error to the user, etc
-      }
-    });
-
-// turn on bluetooth ourself if we can
-// for iOS, the user controls bluetooth enable/disable
-    if (Platform.isAndroid) {
-      await FlutterBluePlus.turnOn();
-    }
-
-// cancel to prevent duplicate listeners
-    subscription.cancel();
-  }
-
-  // void scanForDevices() async {
-  //   // Check Bluetooth state
-  //   if (!await FlutterBluePlus.instance.isOn) {
-  //     await FlutterBluePlus.instance.turnOn();
-  //   }
-  //
-  //   // Start scanning
-  //   FlutterBluePlus.instance.scanForDevices(withServices: []).listen((device) {
-  //     setState(() {
-  //       devices.add(device);
-  //     });
-  //   });
-  // }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bluetooth Devices'),
-        ),
-        body: ListView.builder(
-          itemCount: devices.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(devices[index].name ?? 'Unknown Device'),
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(),
     );
   }
 }
 
-// import 'package:ble_scan/views/homePage.dart';
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(),
-//     );
-//   }
-// }
-//
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("BLE SCANNER"),),
+        body: GetBuilder<BleController>(
+          init: BleController(),
+          builder: (BleController controller)
+          {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<List<ScanResult>>(
+                      stream: controller.scanResults,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Expanded(
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: (context, index) {
+                                  final data = snapshot.data![index];
+                                  return Card(
+                                    elevation: 2,
+                                    child: ListTile(
+                                      title: Text(data.device.name),
+                                      subtitle: Text(data.device.id.id),
+                                      trailing: Text(data.rssi.toString()),
+                                      onTap: ()=> controller.connectToDevice(data.device),
+                                    ),
+                                  );
+                                }),
+                          );
+                        }else{
+                          return Center(child: Text("No Device Found"),);
+                        }
+                      }),
+                  SizedBox(height: 10,),
+                  ElevatedButton(onPressed: ()  async {
+                    controller.scanDevices();
+                    // await controller.disconnectDevice();
+                  }, child: Text("SCAN")),
+
+                ],
+              ),
+            );
+          },
+        )
+    );
+  }
+}
